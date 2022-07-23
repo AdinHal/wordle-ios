035f5a13ae5a036db6f68893bd5ca34e2f5f3380
Adin Halilovic <adin.halilovic15@gmail.com>
2022-07-23 22:09:56 +0200

    Game Completed

---
+++ Wordle/Core/KeyCell.swift
@@ -0,0 +1,46 @@
+//
+//  KeyCell.swift
+//  Wordle
+//
+//  Created by Adi on 23.07.22.
+//
+
+import UIKit
+
+class KeyCell: UICollectionViewCell {
+    static let identifier = "KeyCell"
+    
+    let label : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemGray
+        contentView.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+    }
+    
+    func configure(with letter: Character){
+        label.text = String(letter).uppercased()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        label.text = nil
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
